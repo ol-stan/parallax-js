7b0bcd5fddd7e4ac5b2256ae2e1ccace4c8f70d3
ol-stan <olstan125@gmail.com>
2020-03-19 20:52:38 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+
+//     function heightDetect() {
+//         $(".parallax").css("height", $(window).height());
+//     };
+
+//     heightDetect();
+//     $(window).resize(function () {
+//         heightDetect();
+//     });
+// });
